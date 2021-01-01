a70d182e33fb8f605f7bb6d18ebddd4eddd2d4db
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2021-01-01 08:15:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>IGStoryDL-Automated</h1>
+
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/) [![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+
+<h3>What is this repo about?</h3>
+<p>This is a simple script written in python to automate downloading stroies from Instagram. This will send you the downloaded files via a Telegram Bot<p>
+
+<h3>How to deploy?</h3>
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/NandiyaLive/IGStoryDL-Automated)
+
+1) Fork this repo.
+2) Edit bot.py
+   `token` : Bot token from the 
+   `chat_id`
+   `user_ids`
+   `src_dir`
+<br>
+© Neranjana Prasad 2020.
---
+++ app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+# Coded with ❤️ by Neranjana Prasad (@NandiyaLive)
+
+
+from instaloader import Instaloader
+import shutil
+import glob
+import os
+from telegram import Bot
+from flask import Flask
+
+bot = Bot(token="")
+chat_id = ""
+user_ids = []
+src_dir = ""
+USER = ""
+PASSWORD = ""
+
+app = Flask(__name__)
+
+L = Instaloader(dirname_pattern=src_dir, download_comments=False,
+                download_video_thumbnails=False, save_metadata=False, download_geotags=True, compress_json=True, post_metadata_txt_pattern=None, storyitem_metadata_txt_pattern=None)
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def job():
+    try:
+        L.download_stories(userids=user_ids)
+
+        for jpgfile in glob.iglob(os.path.join(src_dir, "*.jpg")):
+            bot.send_photo(chat_id, photo=open(jpgfile, 'rb'))
+
+        for vidfile in glob.iglob(os.path.join(src_dir, "*.mp4")):
+            bot.send_video(chat_id, video=open(vidfile, 'rb'))
+
+        try:
+            shutil.rmtree(src_dir)
+        except Exception:
+            pass
+        return 'Download Success'
+
+    except:
+        L.login(USER, PASSWORD)
+        job()
